abc992943534dd497f29b031198adab3106327df
JanHuang <bboyjanhuang@gmail.com>
2017-04-06 17:32:32 +0800

    update controller

--- config/routes.php
+++ config/routes.php
@@ -1,4 +1,10 @@
 <?php
 
-route()->get(['/', 'name' => 'welcome'], 'IndexController@welcome');
-route()->get(['/hello/{name}', 'name' => 'hello'], 'IndexController@sayHello');
+route()->group('/api', function () {
+    route()->get('/spaces', 'SpaceController@select');
+    route()->post('/spaces', 'SpaceController@create');
+    route()->get('/spaces/{id}', 'SpaceController@find');
+    route()->patch('/spaces/{id}', 'SpaceController@patch');
+    route()->delete('/spaces/{id}', 'SpaceController@delete');
+    route()->get('/medias', '');
+});
